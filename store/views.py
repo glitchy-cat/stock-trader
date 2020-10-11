51c6e0766a6d3e9b58d4f7daa4bf8a2d6fcfa6f4
Jimmy Bull <jimmybull18@gmail.com>
2020-10-11 17:59:44 -0400

    Added python function to views to handle api call

--- store/views.py
+++ store/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'home.html')
+    import requests
+    import json
+
+    # pk_be2bfc4758174ecea5b2405177a5d321
+    api_request = requests.get("https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_be2bfc4758174ecea5b2405177a5d321")
+
+    try:
+        api = json.loads(api_request.content)
+
+    except Exception as e:
+        api = "Error..."
+
+    return render(request, 'home.html', {'api':api})
 
 
 def about(request):
