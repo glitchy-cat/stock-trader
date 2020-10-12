6f9d4267e7353cdb501762c1db65608db03b2624
Jimmy Bull <jimmybull18@gmail.com>
2020-10-11 21:26:16 -0400

    Added stock portfolio html page to templates

--- store/views.py
+++ store/views.py
@@ -23,4 +23,8 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 
+
+def add_stock(request):
+    return render(request, 'add_stock.html')
+
 # Create your views here.
